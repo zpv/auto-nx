77a1bd00fd43b1dcff89c6813b73012fe6fcbb68
Steven Zhao <steven@zhao.io>
2020-05-18 23:22:07 -0400

    Add explanation

--- src/scripts/Gtop100/index.js
+++ src/scripts/Gtop100/index.js
@@ -26,9 +26,12 @@ async function vote() {
   console.log(captchaToken);
   console.log(url);
   const params = new URLSearchParams();
+
   params.append('site', '93369');
   params.append('pingUsername', pingUsername);
   params.append('fcToken', captchaToken);
+
+  // these fields don't actually matter
   params.append('fingerprintid', 'a50283ba54286ffa9e9e00de8b6c323a');
   params.append('minecraftname', 'America/Toronto');
   params.append('tz', 'undefined');
@@ -36,10 +39,12 @@ async function vote() {
     'reToken',
     '03AGdBq279gRWUrUf8haaqE_NDRHgflp-ZaxknI1y1Hbk7LgnqBDmDfXZqsRGWMjtqYgcodCZ-RJBL3DlPz06ewasqj2tUZLfgfoeg3sN9X8p7ML-fSgiP6Tw38mRe4B6siCdqQ6u9L7HhrfD6zRT_Sd0Yg_qNwPaffrI-9lLNLnWmdmJo2qZcvE5VeT_b99SUMSeCqutUN2GWsMW-4uXhwWSkP36R3hC_FQ6EiUUP5dihk9AeoKovyBxQPWckInRijyDykvameUixOIfD6xaudEO4WlgVZ03qVZgCLBC26S6u-FZ5zSSZjsdb-ymT4gbkcQMhKSLf_mwkn2v9qn5A9GzH9mEJm1bVpv4ZPM34vgJkqAmBVc02ES1QRriE9noYlttShSnbnhti'
   );
+
   const { data } = await http.client.post(
     'https://gtop100.com/home/send_vote',
     params
   );
+
   console.log(data);
   console.log('[Gtop100] Voted.');
 }
